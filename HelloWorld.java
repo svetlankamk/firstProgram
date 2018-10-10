fea0ce669a32c32d813bec691b41ef9cf5eece6a
svetlankamk <kovalevichag@gmail.com>
2018-10-10 17:11:47 +0300

    89564646465461232146

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,16 +7,5 @@ public class HelloWorld {
     public static void main(String [] args){
         InputStreamReader is = new InputStreamReader(System.in);
         BufferedReader bis = new BufferedReader(is);
-
-        try {
-            System.out.println("Введите ваше имя и нажмите клавишу ЕНТЕР =---");
-            String name = bis.readLine();
-            System.out.println("Привет, уважаемый гость - "+name);
-        } catch (IOException e) {
-            System.out.println("ошибка");
-        }
-//dfdfd
-        //sdfdsfpodskjfpodsfdsf
-        //sdfdsf
     }
 }
